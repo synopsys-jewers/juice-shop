e7cc3bcd00bcf12b81d0d06489f4d06b86dc378f
Tommy Vernieri <Tommy.Vernieri@blackbaud.me>
2019-10-16 15:04:07 -0400

    Paramerize SQL
    CTF Team - TommyAndChristi

--- routes/login.js
+++ routes/login.js
@@ -21,7 +21,17 @@ module.exports = function login () {
 
   return (req, res, next) => {
     verifyPreLoginChallenges(req)
-    models.sequelize.query('SELECT * FROM Users WHERE email = \'' + (req.body.email || '') + '\' AND password = \'' + insecurity.hash(req.body.password || '') + '\' AND deletedAt IS NULL', { model: models.User, plain: true })
+    models.sequelize.query(
+      'SELECT * FROM Users WHERE email = :email AND password = :password AND deletedAt IS NULL',
+      {
+        model: models.User,
+        plain: true,
+        replacements: {
+          email: req.body.email,
+          password: insecurity.hash(req.body.password || '')
+        }
+      }
+    )
       .then((authenticatedUser) => {
         let user = utils.queryResultToJson(authenticatedUser)
         const rememberedEmail = insecurity.userEmailFrom(req)
