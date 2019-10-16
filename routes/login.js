ca22300eee4d32c38582edd47364350bcbea9d0f
Gavin Nicol <Gavin.Nicol@blackbaud.com>
2019-10-16 11:06:51 -0400

    fix SQL injection on login

--- routes/login.js
+++ routes/login.js
@@ -21,7 +21,9 @@ module.exports = function login () {
 
   return (req, res, next) => {
     verifyPreLoginChallenges(req)
-    models.sequelize.query('SELECT * FROM Users WHERE email = \'' + (req.body.email || '') + '\' AND password = \'' + insecurity.hash(req.body.password || '') + '\' AND deletedAt IS NULL', { model: models.User, plain: true })
+    models.sequelize.query(
+      'SELECT * FROM Users WHERE email = :email AND password = :password AND deletedAt IS NULL',
+      { replacements: { email: (req.body.email || ''), password: insecurity.hash(req.body.password || '') }, type: sequelize.QueryTypes.SELECT, model: models.User, plain: true })
       .then((authenticatedUser) => {
         let user = utils.queryResultToJson(authenticatedUser)
         const rememberedEmail = insecurity.userEmailFrom(req)
